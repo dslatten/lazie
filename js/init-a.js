48574d35cb2e5aa4d5803b6d993acfb49ad68eb1
Darren Slatten <darrenslatten@gmail.com>
2013-04-10 21:58:31 -0500

    Adjust relative links

--- js/init-a.js
+++ js/init-a.js
@@ -4,7 +4,7 @@
 
 (function() {
 
-    console.log('init-a.js is executing...')
+    console.log('Begin init-a.js IIFE');
 
     // RequireJS config
     require = {
@@ -15,29 +15,29 @@
 
             // RequireJS loader plugins
             text: [
-                '/lib/require-text/text'
+                'lib/require-text/text'
             ],
 
             // Durandal modules
             durandal: [
-                '/lib/durandal'
+                'lib/durandal'
             ],
             samples: [
-                '/app/samples'
+                'app/samples'
             ],
 
             // CDN-hosted 3rd-party libs + local fallbacks
             jQuery: [
                 '//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min',
-                '/lib/jquery/jquery'
+                'lib/jquery/jquery'
             ],
             knockout: [
                 '//cdnjs.cloudflare.com/ajax/libs/knockout/2.2.1/knockout-min',
-                '/lib/knockout/knockout'
+                'lib/knockout/knockout'
             ],
             sammy: [
                 '//cdnjs.cloudflare.com/ajax/libs/sammy.js/0.7.4/sammy.min',
-                '/lib/sammy/sammy'
+                'lib/sammy/sammy'
             ]
 
         },
@@ -53,7 +53,8 @@
 
 }());
 
-console.log('Immediately-invoked function is finished.');
+console.log('End init-a.js IIFE');
+
 
 /* ========================================================================== *\
     Execute on page load
@@ -61,13 +62,15 @@ console.log('Immediately-invoked function is finished.');
 
 var lazyFunction = function() {
 
-    console.log('lazyFunction is executing...');
+    console.log('Begin init-a.js lazyFunction');
 
     // Load RequireJS
     var s = document.createElement('script');
     s.type = 'text/javascript';
     s.src = 'lib/require/require.js';
     document.getElementsByTagName('head')[0].appendChild(s);
+
+    console.log('End init-a.js lazyFunction');
 
 }
 
@@ -78,10 +81,14 @@ var lazyFunction = function() {
 
 var oldOnload = window.onload;
 if (typeof window.onload != 'function') {
-    window.onload = lazyFunction;
+    window.onload = function() {
+        console.log('window.onload');
+        lazyFunction();
+    }
 }
 else {
     window.onload = function() {
+        console.log('window.onload');
         lazyFunction();
         if (oldOnload) {
             oldOnload();
